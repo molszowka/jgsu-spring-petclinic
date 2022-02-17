ab6b47dd826e0f4f55cd9f858de0a202dedb31a5
molszowka <marek.olszowka@britenet.com.pl>
2022-02-17 10:34:45 +0000

    break a test

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -34,14 +34,12 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 class ValidatorTests {
 
-	private Validator createValidator() {
+	pre Validator createValidator() {
 		LocalValidatorFactoryBean localValidatorFactoryBean = new LocalValidatorFactoryBean();
 		localValidatorFactoryBean.afterPropertiesSet();
 		return localValidatorFactoryBean;
-	}
-
 	@Test
-	void shouldNotValidateWhenFirstNameEmpty() {
+	vd shouldNotValidateWhenFirstNameEmpty() {
 
 		LocaleContextHolder.setLocale(Locale.ENGLISH);
 		Person person = new Person();
